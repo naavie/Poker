1883cd281d6259e850392cb1e8bc85872ad2551f
naavie <84877044+naavie@users.noreply.github.com>
2022-04-11 18:57:13 -0400

    Organizing thoughts, nothing else.

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+var playerOneHand string = "Black:"
+var playerTwoHand string = "White:"
+
+// Example: A hand is represented by a slice of cards, size 5
+// -----
+//			Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH
+
+// Function: Turing a string into a card: How do I turn the string into a card?
+
+//- The first character of a card string is its number and the second character is the card’s suit.
+
+// - To convert a string into a card, we can create a new card and select the 0th and 1st elements.
+// - The 0th element can be used to set the card’s number and the 1st element can be used to set the card’s suit.
+
+func newCard() {
+
+	var playerOneCards string
+	var playerTwoCards string
+	for i := 0; i <= 5; i++ {
+
+	}
+}
