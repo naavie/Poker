c092429203cd8bb4d041cb3e144e8fa3fcd285bc
naavie <84877044+naavie@users.noreply.github.com>
2022-04-12 18:09:53 -0400

    Update takehome.go

--- takehome.go
+++ takehome.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Take Home: Using the initial hands string, write a text function, composed of other functions that turns it into two hands that are identifiable into white hand and black hand.
@@ -10,32 +11,41 @@ import (
 
 func main() {
 	//  Variables that contain the original card set we were working with.
-	var BlackHand [5]string
+	BlackHand := []string{"2H", "3D", "5S", "9C", "KD"}
 
-	BlackHand[0] = "2H"
-	BlackHand[1] = "3D"
-	BlackHand[2] = "5S"
-	BlackHand[3] = "9C"
-	BlackHand[4] = "KD"
+	// BlackHand[0] = "2H"
+	// BlackHand[1] = "3D"
+	// BlackHand[2] = "5S"
+	// BlackHand[3] = "9C"
+	// BlackHand[4] = "KD"
 
-	var WhiteHand [5]string
+	WhiteHand := []string{"2C", "3H", "4S", "8C", "AH"}
 
-	WhiteHand[0] = "2C"
-	WhiteHand[1] = "3H"
-	WhiteHand[2] = "4S"
-	WhiteHand[3] = "8C"
-	WhiteHand[4] = "AH"
+	// WhiteHand[0] = "2C"
+	// WhiteHand[1] = "3H"
+	// WhiteHand[2] = "4S"
+	// WhiteHand[3] = "8C"
+	// WhiteHand[4] = "AH"
 
 	// The print statements below should match the expected output indicated in line 7.
 
 	playerOne := "Black:"
 	playerTwo := "White:"
 
+	// Testing Pt. 1: Code works this far.
 	fmt.Println(playerOne, BlackHand)
 	fmt.Println(playerTwo, WhiteHand)
 
-	// Printed on same line
+	// Testing Pt. 1: Continued.
 	fmt.Println(playerOne, BlackHand, playerTwo, WhiteHand)
+
+	BlackHandStrings := strings.Join(BlackHand, ", ")
+	WhiteHandStrings := strings.Join(WhiteHand, ", ")
+
+	fmt.Println(BlackHandStrings)
+	fmt.Println(WhiteHandStrings)
+
+	// Final steps
 
 }
 
@@ -46,3 +56,7 @@ func main() {
 //																					   ["Black:","2H","3D","5S","9C","KD","White:","2C","3H","4S","8C","AH"]
 
 // SPLIT function
+
+func Split(string) {
+
+}
