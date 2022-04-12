a77bf4752bd679a0ae19fe37ad86e1bb5dc24e6d
naavie <84877044+naavie@users.noreply.github.com>
2022-04-12 16:30:25 -0400

    Update takehome.go

--- takehome.go
+++ takehome.go
@@ -8,31 +8,38 @@ import "fmt"
 
 func main() {
 	//  Variables that contain the original card set we were working with.
-	var originalBlackHand [5]string
+	var BlackHand [5]string
 
-	originalBlackHand[0] = "2H"
-	originalBlackHand[1] = "3D"
-	originalBlackHand[2] = "5S"
-	originalBlackHand[3] = "9C"
-	originalBlackHand[4] = "KD"
+	BlackHand[0] = "2H"
+	BlackHand[1] = "3D"
+	BlackHand[2] = "5S"
+	BlackHand[3] = "9C"
+	BlackHand[4] = "KD"
 
-	var originalWhiteHand [5]string
+	var WhiteHand [5]string
 
-	originalWhiteHand[0] = "2C"
-	originalWhiteHand[1] = "3H"
-	originalWhiteHand[2] = "4S"
-	originalWhiteHand[3] = "8C"
-	originalWhiteHand[4] = "AH"
+	WhiteHand[0] = "2C"
+	WhiteHand[1] = "3H"
+	WhiteHand[2] = "4S"
+	WhiteHand[3] = "8C"
+	WhiteHand[4] = "AH"
 
 	// The print statements below should match the expected output indicated in line 7.
 
 	playerOne := "Black:"
 	playerTwo := "White:"
 
-	fmt.Println(playerOne, originalBlackHand)
-	fmt.Println(playerTwo, originalWhiteHand)
+	fmt.Println(playerOne, BlackHand)
+	fmt.Println(playerTwo, WhiteHand)
+
+	fmt.Println(playerOne, BlackHand, playerTwo, WhiteHand)
+
 }
 
 // Task One: Separate every element in originalBlackHand & originalWhiteHand with the SPLIT function such that there is a comma between the former and latter elements
+
+// - Using the split function one additional time, this is the output you would expect:
+
+//																					   ["Black:","2H","3D","5S","9C","KD","White:","2C","3H","4S","8C","AH"]
 
 // SPLIT function
