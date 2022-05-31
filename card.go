ac5558bfb53f8436dd86b6492160091dbdf5b1b8
naavie <84877044+naavie@users.noreply.github.com>
2022-05-31 16:08:25 -0400

    Updated card.go file syntax

--- card.go
+++ card.go
@@ -1,34 +1,27 @@
 package main
 
-// Card Constructer - Takes a string such as "2H" that returns a card object that returns a number and a suit.
-// The number should be comparable to the >, <, ,etc and the suit should have equality.
-
-// Example of Constraint: Given card, 10H, return
-//{ Rank:ten, Suit: hearts}
-
+// Type: Card
 type Card struct {
 	rank Rank
 	suit Suit
 }
 
-// Strings are slices, you can pick out parts by normal slice mechanics... str[2:] (give me everything from the second to the last) or str[1:len(str)] (second to the last).
-
+// Function: CardConstructer - Takes a string such as "2H" that returns a card object that returns a number and a suit.
 func CardConstructer(card string) Card {
-	// example := "10H"
 	cardSuit := card[len(card)-1]
 	cardNumber := card[:len(card)-1]
 
-	return Card{rank: ranks[cardNumber], suit: suits[cardSuit]}
-
-	// turn the variable example into 10 & H, separately. Output: { Rank:ten, Suit: hearts}
+	return Card{rank: ranks[cardNumber], suit: suits[cardSuit]} //This will return a card of { Rank:ten, Suit: hearts}
 
 }
 
+// Type: Hand
 type Hand struct {
 	cards      []Card
 	playerName string
 }
 
+// Function: HandConstructer - Takes two inputs, player name (string) and a slice of cards, and returns a Hand(Type) with values {cards: initalHand, playerName: playerName}.
 func HandConstructer(playerName string, cards []string) Hand {
 	initalHand := make([]Card, 0)
 
@@ -40,20 +33,38 @@ func HandConstructer(playerName string, cards []string) Hand {
 
 }
 
+// Type: Suit - There are four, different suits in a standard deck of cards.
 type Suit struct {
 	suit string
 }
 
 var suits = map[byte]Suit{
-	'H': Suit{"Hearts"},
-	'S': Suit{"Spades"},
-	'C': Suit{"Clubs"},
-	'D': Suit{"Diamonds"},
+	'H': {"Hearts"},
+	'S': {"Spades"},
+	'C': {"Clubs"},
+	'D': {"Diamonds"},
 }
 
+// Type: Rank - There are 13 different potential card numbers that a player can draw from a standand deck of cards.
 type Rank struct {
 	value int
 	name  string
+}
+
+var ranks = map[string]Rank{
+	"2":  {2, "Two"},
+	"3":  {3, "Three"},
+	"4":  {4, "Four"},
+	"5":  {5, "Five"},
+	"6":  {6, "Six"},
+	"7":  {7, "Seven"},
+	"8":  {8, "Eight"},
+	"9":  {9, "Nine"},
+	"10": {10, "Ten"},
+	"J":  {11, "Jack"},
+	"Q":  {12, "Queen"},
+	"K":  {13, "King"},
+	"A":  {14, "Ace"},
 }
 
 func (r Rank) LessThan(r2 Rank) bool {
@@ -62,22 +73,6 @@ func (r Rank) LessThan(r2 Rank) bool {
 
 func (r Rank) GreaterThan(r2 Rank) bool {
 	return r.value > r2.value
-}
-
-var ranks = map[string]Rank{
-	"2":  Rank{2, "Two"},
-	"3":  Rank{3, "Three"},
-	"4":  Rank{4, "Four"},
-	"5":  Rank{5, "Five"},
-	"6":  Rank{6, "Six"},
-	"7":  Rank{7, "Seven"},
-	"8":  Rank{8, "Eight"},
-	"9":  Rank{9, "Nine"},
-	"10": Rank{10, "Ten"},
-	"J":  Rank{11, "Jack"},
-	"Q":  Rank{12, "Queen"},
-	"K":  Rank{13, "King"},
-	"A":  Rank{14, "Ace"},
 }
 
 // If you feel you understand the code, the next step is converting from the whole hands string (the one with the player names) into two hands.
